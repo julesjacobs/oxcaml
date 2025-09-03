ceffef280a94663d83a9d97c524eb278d952c19d
julesjacobs <julesjacobs@gmail.com>
2025-09-03 16:48:20 -0400

    Update ikind.ml

--- typing/ikinds/ikind.ml
+++ typing/ikinds/ikind.ml
@@ -1,3 +1,8 @@
-let sub_jkind_l ~type_equal ~context sub super =
+let sub_jkind_l
+    ~(type_equal : Types.type_expr -> Types.type_expr -> bool)
+    ~(context : Jkind.jkind_context)
+    (sub : Types.jkind_l)
+    (super : Types.jkind_l)
+    : (unit, Jkind.Violation.t) result =
   print_endline "sub_jkind_l";
   Jkind.sub_jkind_l ~type_equal ~context sub super
