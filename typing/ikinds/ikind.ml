0394f2391d635991cba2688ca5a3615880643680
julesjacobs <julesjacobs@gmail.com>
2025-09-03 16:52:32 -0400

    Update ikind.ml

--- typing/ikinds/ikind.ml
+++ typing/ikinds/ikind.ml
@@ -6,3 +6,47 @@ let sub_jkind_l
     : (unit, Jkind.Violation.t) result =
   print_endline "sub_jkind_l";
   Jkind.sub_jkind_l ~type_equal ~context sub super
+
+(* Minimal kind constructor over real Types.type_expr, inspired by infer6. *)
+
+module TyM = struct
+  type t = Types.type_expr
+
+  let compare (t1 : t) (t2 : t) = Int.compare (Types.get_id t1) (Types.get_id t2)
+
+  (* Avoid depending on Printtyp to prevent module cycles. *)
+  let to_string (t : t) : string = Printf.sprintf "ty#%d" (Types.get_id t)
+end
+
+module ConstrM = struct
+  type t = Path.t
+
+  let compare = Stdlib.compare
+
+  let to_string (p : t) : string = Format.asprintf "%a" Path.print p
+end
+
+module JK = Ldd_jkind_solver.Make (Axis_lattice) (TyM) (ConstrM)
+
+let kind_of (ty : Types.type_expr) : JK.ckind =
+ fun (ops : JK.ops) ->
+  match Types.get_desc ty with
+  | Types.Tvar _ | Types.Tunivar _ -> ops.rigid ty
+  | Types.Tconstr (p, args, _) ->
+    let arg_kinds = List.map (fun t -> ops.kind_of t) args in
+    ops.constr p arg_kinds
+  | Types.Ttuple elts ->
+    elts |> List.map (fun (_lbl, t) -> ops.kind_of t) |> ops.join
+  | Types.Tunboxed_tuple elts ->
+    elts |> List.map (fun (_lbl, t) -> ops.kind_of t) |> ops.join
+  | Types.Tarrow (_lbl, t1, t2, _commu) -> ops.join [ ops.kind_of t1; ops.kind_of t2 ]
+  | Types.Tlink t -> ops.kind_of t
+  | Types.Tsubst (_, _)
+  | Types.Tpoly (_, _)
+  | Types.Tof_kind _
+  | Types.Tobject (_, _)
+  | Types.Tfield (_, _, _, _)
+  | Types.Tnil
+  | Types.Tvariant _
+  | Types.Tpackage _ ->
+    failwith "kind_of: unhandled type"
