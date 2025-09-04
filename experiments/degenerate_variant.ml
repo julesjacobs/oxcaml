50a70bf967a6f444d7b9ceb11bc5d4e9afda1839
julesjacobs <julesjacobs@gmail.com>
2025-09-04 14:15:03 -0400

    Update degenerate_variant.ml

--- experiments/degenerate_variant.ml
+++ experiments/degenerate_variant.ml
@@ -1,4 +1,7 @@
-type 'a degenerate : immutable_data with ('a * 'a) =
+type 'a degenerate : immutable_data with 'a =
   | Leaf of 'a
   | Branch of ('a * 'a) degenerate
 
+(* type 'a foo : immutable_data =
+  | Leaf
+  | Branch of ('a list) foo list *)
