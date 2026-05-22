d7359e7b3c0623e7a2329f8373327479471044c3
julesjacobs <julesjacobs@gmail.com>
2026-05-22 17:34:29 -0400

    Add LLVM expect test support

--- backend/llvm/llvmize.mli
+++ backend/llvm/llvmize.mli
@@ -42,3 +42,7 @@ val open_out : asm_filename:string -> unit
 val assemble_file : asm_filename:string -> obj_filename:string -> int
 
 val init : output_prefix:string -> ppf_dump:Format.formatter -> unit
+
+val register_expect_llvm_ir_callback : (string -> unit) -> unit
+
+val register_expect_llvm_asm_callback : (string -> unit) -> unit
