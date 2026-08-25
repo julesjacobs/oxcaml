50b984ab64dcf1ce4e67003ee8fd70466dce2117
julesjacobs <julesjacobs@gmail.com>
2026-08-24 21:07:59 -0400

    Resolve Merlin import for totality

--- external/merlin/src/analysis/mode_enclosing.ml
+++ external/merlin/src/analysis/mode_enclosing.ml
@@ -29,6 +29,7 @@ module Mode_info = struct
            linearity;
            forkable;
            yielding;
+           totality;
            staticity
          }
           : Mode.Value.Const.t) =
@@ -45,6 +46,7 @@ module Mode_info = struct
           maybe_print (module Mode.Linearity.Const) linearity;
           maybe_print (module Mode.Forkable.Const) forkable;
           maybe_print (module Mode.Yielding.Const) yielding;
+          maybe_print (module Mode.Totality.Const) totality;
           maybe_print (module Mode.Staticity.Const) staticity
         ]
         ~f:Fun.id
