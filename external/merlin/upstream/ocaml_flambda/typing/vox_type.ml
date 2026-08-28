47cb8a1d5d7346a2c113508defa1ec57722580ae
julesjacobs <julesjacobs@gmail.com>
2026-08-28 02:09:56 -0400

    Automated commit: Import compiler changes from 3e5b31f51f1b4d7ff99a464c20efd3621a4b6e06

--- external/merlin/upstream/ocaml_flambda/typing/vox_type.ml
+++ external/merlin/upstream/ocaml_flambda/typing/vox_type.ml
@@ -1,11 +1,15 @@
 open Types
 
-type t = Int | Bool
+type t = Int | Bool | Bigint
+
+let bigint_path name =
+  Path.Pdot (Path.Pident (Ident.create_persistent "Stdlib__Bigint"), name)
 
 let classify env ty =
   match get_desc (Ctype.expand_head env ty) with
   | Tconstr (path, [], _) when Path.same path Predef.path_int -> Some Int
   | Tconstr (path, [], _) when Path.same path Predef.path_bool -> Some Bool
+  | Tconstr (path, [], _) when Path.same path (bigint_path "t") -> Some Bigint
   | _ -> None
 
 let rec classify_payload env ty =
