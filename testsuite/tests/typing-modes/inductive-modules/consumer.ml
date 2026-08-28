ba81b36b47a967fd12342a03a7065c62a00543a7
julesjacobs <julesjacobs@gmail.com>
2026-08-28 01:49:26 -0400

    Check structurally decreasing total recursion

--- testsuite/tests/typing-modes/inductive-modules/consumer.ml
+++ testsuite/tests/typing-modes/inductive-modules/consumer.ml
@@ -2,4 +2,8 @@ let (predecessor @ total) = function
   | Definition.Z -> Definition.Z
   | Definition.S n -> n
 
-let (call @ total) n = Definition.predecessor n
+let rec (depth @ total) n =
+  match n with Definition.Z -> 0 | Definition.S x -> 1 + depth x
+
+let (call_predecessor @ total) n = Definition.predecessor n
+let (call_depth @ total) n = Definition.depth n
