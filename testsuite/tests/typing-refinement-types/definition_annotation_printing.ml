ca09f984af937fc93c55058754e09e267d902cf2
julesjacobs <julesjacobs@gmail.com>
2026-09-05 20:51:44 +0200

    Preserve result annotations when simplifying definition predicates

---
+++ testsuite/tests/typing-refinement-types/definition_annotation_printing.ml
@@ -0,0 +1,20 @@
+(* TEST
+ flags = "-extension refinement_types";
+ expect;
+*)
+
+let[@def] eq_body x =
+  let _unused = 0 in ((=) : int -> int -> bool);;
+[%%expect{|
+val eq_body : 'a @ immutable -> int -> int -> bool = <fun>
+val eq_body_def :
+  (x : 'a) -> {u : unit | (eq_body x) === ((=) : int -> int -> bool)} = <fun>
+|}]
+
+let[@def] eq_outer x =
+  (let _unused = 0 in (=) : int -> int -> bool);;
+[%%expect{|
+val eq_outer : 'a @ immutable -> int -> int -> bool = <fun>
+val eq_outer_def :
+  (x : 'a) -> {u : unit | (eq_outer x) === ((=) : int -> int -> bool)} = <fun>
+|}]
