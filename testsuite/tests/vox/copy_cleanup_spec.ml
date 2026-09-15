fa7c9e76dcb852deaaf77b4edc87ab5dd81e0e7a
julesjacobs <julesjacobs@gmail.com>
2026-09-15 22:28:52 +0200

    Clear copy memos using a verified touched-cell trail

---
+++ testsuite/tests/vox/copy_cleanup_spec.ml
@@ -0,0 +1,19 @@
+open Copy_spec
+open Generalize_spec
+
+let[@def] (clear_memo @ total) (v : node @ immutable) =
+  {v with memo = Empty_memo}
+let[@def] rec (swept @ total) (h : node Pref.heap @ immutable)
+    (trail : pool @ immutable) = ghost_ (match trail with
+  | Empty -> h
+  | Entry (p, rest) -> match H.at h p with
+    | None -> swept h rest
+    | Some v -> swept (H.put h p (clear_memo v)) rest)
+let[@def] (swept_at @ total) (h : node Pref.heap @ immutable)
+    (after : node Pref.heap @ immutable) (trail : pool @ immutable)
+    (x : node Pref.t @ immutable) = ghost_ (
+  H.mem h x === H.mem after x && match H.at h x, H.at after x with
+  | None, None -> true
+  | Some a, Some b -> a.desc === b.desc && a.level === b.level
+    && b.memo === (if listed trail x then Empty_memo else a.memo)
+  | _ -> false)
