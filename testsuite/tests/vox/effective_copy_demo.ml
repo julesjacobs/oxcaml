a74f17625a115167992d41f442adda324eca361b
julesjacobs <julesjacobs@gmail.com>
2026-09-17 15:49:03 +0200

    Prove the effective-level HM runtime invariant

--- testsuite/tests/vox/effective_copy_demo.ml
+++ testsuite/tests/vox/effective_copy_demo.ml
@@ -2,7 +2,7 @@
  has-z3;
  flags = "-extension refinement_types";
  source_directories = "${test_source_directory}/../../../verification/library";
- all_modules = "pref.mli pref.ml copy_spec.ml copy_heap_proofs.ml copy_model_proofs.ml copy_complete_proofs.ml copy_sound_proofs.ml copy_template_proofs.ml copy_algorithm.ml level_spec.ml lower_locality_spec.ml level_proofs.ml lower_locality_proofs.ml level_lower.ml level_unifier_spec.ml marked_occurs_proofs.ml level_unifier_proofs.ml level_unifier_metadata.ml marked_occurs.ml level_unifier.ml level_copy_proofs.ml generalize_spec.ml generalize_proofs.ml generalize_scheme_proofs.ml generalize.ml level_finite_spec.ml level_finite_proofs.ml level_mgu_spec.ml level_mgu_proofs.ml forest_transport.ml pooled_spec.ml pooled_proofs.ml pooled_allocation_proofs.ml pooled_allocator.ml pooled_copy.ml provenance_spec.ml provenance_proofs.ml relative_generalization.ml compression_model_proofs.ml compression_finite_proofs.ml hm_declarative.ml hm_environment_spec.ml compression_spec.ml compression_path_proofs.ml compressed_representative.ml compression_proofs.ml leaf_provenance_spec.ml leaf_provenance_proofs.ml structure_finite_proofs.ml structure_model_proofs.ml structure_spec.ml structure_link.ml structure_origin_proofs.ml compression_origin_proofs.ml optimized_unifier_spec.ml optimized_metadata.ml optimized_finite_proofs.ml optimized_model_proofs.ml optimized_origin_proofs.ml optimized_link_proofs.ml optimized_link.ml pruned_lower_proofs.ml pruned_lower.ml pruned_bind.ml optimized_unifier.ml copy_order_proofs.ml ordered_copy.ml copy_cleanup_spec.ml copy_cleanup_proofs.ml copy_cleanup.ml clean_pooled_copy.ml clean_copy.ml nested_pool_spec.ml nested_pool_proofs.ml nested_pool.ml representative_level.ml representative_pool_spec.ml representative_pool_proofs.ml representative_pool.ml effective_level.ml effective_template.ml representative_mutation.ml effective_copy_spec.ml effective_copy_heap_proofs.ml effective_copy_metadata.ml effective_copy_complete.ml effective_copy_sound.ml effective_copy_template.ml effective_copy_finite.ml effective_copy_order.ml effective_copy_origin.ml effective_copy_pool.ml effective_copy_runtime.ml representative_certificate.ml copy_certificate_spec.ml copy_certificate_capture.ml copy_certificate_proofs.ml certified_copy.ml hm_type_proofs.ml hm_abstraction.ml hm_abstraction_proofs.ml hm_substitution.ml hm_substitution_proofs.ml hm_freshness_proofs.ml hm_template_instance_proofs.ml hm_execution_spec.ml hm_effective_environment.ml hm_effective_variable.ml effective_lower_spec.ml effective_lower_proofs.ml effective_lower_paths.ml effective_lower_tree.ml effective_lower_write.ml terminal_lower_spec.ml terminal_lower_proofs.ml effective_lower_runtime.ml graph_occurs.ml effective_compression_spec.ml effective_compression_proofs.ml effective_unifier_spec.ml effective_unifier_model.ml effective_unifier_finite.ml effective_unifier_frame.ml effective_unifier_mgu.ml effective_scan_proofs.ml effective_bind_proofs.ml effective_bind.ml effective_compression_metadata.ml effective_unifier_heads.ml effective_unifier_metadata.ml effective_unifier_order.ml graph_representative.ml effective_compressed_representative.ml effective_link_proofs.ml effective_link.ml effective_unifier_runtime.ml forest_heads.ml hm_effective_execution_spec.ml hm_effective_forest.ml hm_effective_model.ml effective_copy_demo.ml";
+ all_modules = "pref.mli pref.ml copy_spec.ml copy_heap_proofs.ml copy_model_proofs.ml copy_complete_proofs.ml copy_sound_proofs.ml copy_template_proofs.ml copy_algorithm.ml level_spec.ml lower_locality_spec.ml level_proofs.ml lower_locality_proofs.ml level_lower.ml level_unifier_spec.ml marked_occurs_proofs.ml level_unifier_proofs.ml level_unifier_metadata.ml marked_occurs.ml level_unifier.ml level_copy_proofs.ml generalize_spec.ml generalize_proofs.ml generalize_scheme_proofs.ml generalize.ml level_finite_spec.ml level_finite_proofs.ml level_mgu_spec.ml level_mgu_proofs.ml forest_transport.ml pooled_spec.ml pooled_proofs.ml pooled_allocation_proofs.ml pooled_allocator.ml pooled_copy.ml provenance_spec.ml provenance_proofs.ml relative_generalization.ml compression_model_proofs.ml compression_finite_proofs.ml hm_declarative.ml hm_environment_spec.ml compression_spec.ml compression_path_proofs.ml compressed_representative.ml compression_proofs.ml leaf_provenance_spec.ml leaf_provenance_proofs.ml structure_finite_proofs.ml structure_model_proofs.ml structure_spec.ml structure_link.ml structure_origin_proofs.ml compression_origin_proofs.ml optimized_unifier_spec.ml optimized_metadata.ml optimized_finite_proofs.ml optimized_model_proofs.ml optimized_origin_proofs.ml optimized_link_proofs.ml optimized_link.ml pruned_lower_proofs.ml pruned_lower.ml pruned_bind.ml optimized_unifier.ml copy_order_proofs.ml ordered_copy.ml copy_cleanup_spec.ml copy_cleanup_proofs.ml copy_cleanup.ml clean_pooled_copy.ml clean_copy.ml nested_pool_spec.ml nested_pool_proofs.ml nested_pool.ml representative_level.ml representative_pool_spec.ml representative_pool_proofs.ml representative_pool.ml effective_level.ml effective_template.ml representative_mutation.ml effective_copy_spec.ml effective_copy_heap_proofs.ml effective_copy_metadata.ml effective_copy_complete.ml effective_copy_sound.ml effective_copy_template.ml effective_copy_finite.ml effective_copy_order.ml effective_copy_origin.ml effective_copy_pool.ml effective_copy_runtime.ml representative_certificate.ml copy_certificate_spec.ml copy_certificate_capture.ml copy_certificate_proofs.ml certified_copy.ml hm_type_proofs.ml hm_abstraction.ml hm_abstraction_proofs.ml hm_substitution.ml hm_substitution_proofs.ml hm_freshness_proofs.ml hm_template_instance_proofs.ml hm_execution_spec.ml hm_effective_environment.ml hm_effective_variable.ml effective_lower_spec.ml effective_lower_proofs.ml effective_lower_paths.ml effective_lower_tree.ml effective_lower_write.ml terminal_lower_spec.ml terminal_lower_proofs.ml effective_lower_runtime.ml graph_occurs.ml effective_compression_spec.ml effective_compression_proofs.ml effective_unifier_spec.ml effective_unifier_model.ml effective_unifier_finite.ml effective_unifier_frame.ml effective_unifier_mgu.ml effective_scan_proofs.ml effective_bind_proofs.ml effective_bind.ml effective_compression_metadata.ml effective_unifier_heads.ml effective_unifier_metadata.ml effective_unifier_order.ml graph_representative.ml effective_compressed_representative.ml effective_link_proofs.ml effective_link.ml effective_unifier_runtime.ml forest_heads.ml hm_effective_execution_spec.ml hm_effective_forest.ml hm_effective_model.ml effective_unifier_pool.ml hm_effective_runtime.ml hm_effective_allocation.ml effective_allocator.ml hm_effective_copy_runtime.ml hm_effective_registration.ml hm_effective_bound.ml hm_effective_closing.ml effective_copy_demo.ml";
  { bytecode; }
  { native; }
 *)
@@ -115,7 +115,21 @@ let run generic =
       let u = () in refine_ u in
     let order : ((x : node Pref.t) @ immutable -> {u : unit | E.effective_ordered c.C.saved heads.Ghost.ghost x}) @ total = fun x ->
       E.effective_ordered_def c.C.saved heads.Ghost.ghost x; let u = () in refine_ u in
+    let facts : ((x : node Pref.t) @ immutable -> {u : unit |
+        Hm_effective_runtime.runtime_at c.C.saved heads.Ghost.ghost depth base x}) @ total = fun x ->
+      witness.Ghost.ghost x; full_scope x; bounds x; order x;
+      Hm_effective_runtime.runtime_at_def c.C.saved heads.Ghost.ghost depth base x;
+      Hm_effective_runtime.safe_def c.C.saved heads.Ghost.ghost x;
+      Hm_effective_runtime.depth_bound_def c.C.saved heads.Ghost.ghost depth x;
+      let cut = depth - 1 in R.representative_covered_def c.C.saved cut base x;
+      U.terminal_def c.C.saved x; U.observe_def c.C.saved x;
+      covered_def c.C.saved cut base x; Level_spec.at_level_def c.C.saved x;
+      let u = () in refine_ u in
     let u = () in
+    Hm_effective_copy_runtime.copy_runtime c.C.saved heads.Ghost.ghost
+      next_heads.Ghost.ghost depth base facts root d (refine_ raw_witness) result (refine_ u);
+    Hm_effective_copy_runtime.copy_runtime c.C.saved heads.Ghost.ghost
+      next_heads.Ghost.ghost depth base facts root d (refine_ raw_witness) p (refine_ u);
     Effective_copy_order.copy_ordered c.C.saved full_scope heads.Ghost.ghost next_heads.Ghost.ghost witness.Ghost.ghost root depth bounds order d (refine_ raw_witness) result (refine_ u);
     Effective_copy_order.sweep_ordered raw c2.C.saved next_heads.Ghost.ghost trail framing result;
     ());
@@ -142,6 +156,31 @@ let run generic =
   ()
 
 let () = run false; run true
+
+let allocate_stale_alias () =
+  let refine_ state = Pref.empty () in
+  let leaf = {desc = Var; level = Finite 0; memo = Empty_memo; visited = false} in
+  let refine_ step = Pref.alloc leaf state in let q = step.value in
+  let alias = {desc = Link q; level = Finite 7; memo = Empty_memo; visited = false} in
+  let refine_ step = Pref.alloc alias step.state in let p = step.value in
+  let saved : Pref.heap Ghost.t = {Ghost.ghost = ghost_ (Pref.own (borrow_ step.state))} in
+  let desc = Arrow (p, p) in let depth = 1 in let pool = Empty in
+  ghost_ (
+    cell_def desc depth; let v = cell desc depth in payload_scoped_def saved.Ghost.ghost v;
+    pool_scoped_def saved.Ghost.ghost pool;
+    Level_spec.below_def saved.Ghost.ghost p depth;
+    Level_spec.at_level_def saved.Ghost.ghost p;
+    let u = () in let _ : {u : unit | not (Level_spec.below saved.Ghost.ghost p depth)} = refine_ u in ());
+  let refine_ out = Effective_allocator.allocate saved depth desc pool (refine_ step.state) in
+  let state = out.#state in let result = out.#value in
+  ghost_ (
+    let v = cell desc depth in Copy_heap_proofs.put_frame saved.Ghost.ghost result v result; ());
+  let state : {t : Pref.token | H.mem (Pref.own t) result} = refine_ state in
+  let refine_ value = Pref.read result (borrow_ state) in
+  assert (value.level = Finite 1);
+  assert (value.desc = Arrow (p, p))
+
+let () = allocate_stale_alias ()
 
 let[@def] (here @ total) (x : node Pref.t @ immutable total) : R.representative @ immutable total =
   {R.root = x; path = U.Here}
